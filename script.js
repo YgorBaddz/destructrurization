72aef4e7e3f351e86f17b3e61ef3501d4d87dddb
Eegor Badmayev <igorbadmaev2002@gmail.com>
2022-11-17 18:17:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1.
+// const someArray = [1, 2, 3]
+// const [elem1, elem2, elem3, ...arr] = someArray;
+// console.log(elem1, elem2, elem3, 10, 11, 30); 
+
+// 2.
+// const someArray = [1, 2, 3, 4];
+// const [, ,elem1, elem2] = someArray;
+// console.log(elem1, elem2);
+
+// 3.
+// const someArray = [1, 2, 3, 4];
+// const [, elem2, , ] = someArray;
+// console.log(elem2);
